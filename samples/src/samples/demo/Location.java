4915fbdb4a7bdda3c37b84b2b453ea59b3d920d1
Aliah Atiqah <nuraliahatiqah27@gmail.com>
2022-03-10 15:46:06 +0300

    Initial Location Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,16 @@
+package samples.demo;
+
+public class Location {
+	
+	private int location;
+
+	public int getLocation() {
+		return location;
+	}
+
+	public void setLocation(int location) {
+		this.location = location;
+	}
+	
+	
+}
