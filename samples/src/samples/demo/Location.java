32b890add55f66276fb52f14bf8b27082f5ed1bb
Aliah Atiqah <nuraliahatiqah27@gmail.com>
2022-03-10 15:49:09 +0300

    More Attribute Location Commit

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -3,6 +3,24 @@ package samples.demo;
 public class Location {
 	
 	private int location;
+	private int name;
+	public int getName() {
+		return name;
+	}
+
+	public void setName(int name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	private int age;
 
 	public int getLocation() {
 		return location;
